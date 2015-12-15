fa2586ea61cef27f80f489dad2265010e66b3df5
whybruh <b200386@mail.com>
2015-12-15 22:05:27 +0800

    went all out this evening to fix the game

--- gamedev.js
+++ gamedev.js
@@ -1,65 +1,68 @@
 $( document ).ready(function() {
 
 	$("#goRight1").click(function() {
-		$("#exploreBody").css("background-image", "url('img/scary1.gif')");
+		$("#exploreBody").css("background-image", "url('img/two.jpg')");
 		$("#goRight1").hide();
-		$("#goBack").hide();
-		$("#goRight-Straight1").show();
-		$("#goRight-Right2").show();
-	});
-
-	$("#goRight-Straight1").click(function() {
-		$("#exploreBody").css("background-image", "url('img/scary1.gif')");
-		$("#goRight-Straight1").hide();
-		$("#goRight-Straight2").show();
-		$("background-image", "url('img/game.png").hide();
-	});
-
-	$("#goRight-Straight2").click(function() {
-		$("#exploreBody").css("background-image", "url('img/scary1.png')");
-		$("#goRight-Straight2").hide();
-		$("#goRight-Straight3").show();
-	});
-
-	$("#goRight-Straight3").click(function() {
-		$("#exploreBody").css("background-image", "url('img/exploreRight3.jpg')");
-		$("#goRight-Straight3").hide();
-		$("#goRight-Right2").show();
-	});
-
-	$("#goRight-Right2").click(function() {
-		$("#exploreBody").css("background-image", "url('img/explore2-goBack2.jpg')");
-		$("#goRight-Right2").hide();
-		$("#goBack").show();
-	});
-
-
-	$("#goLeft1").click(function() {
-		$("#exploreBody").css("background-image", "url('img/troll.png')");
-		$("#goLeft1").hide();
+		$("#goRight2").show();
 		$("#goLeft2").show();
 	});
 
-	$("#goLeft-Straight1").click(function() {
-		$("#exploreBody").css("background-image", "url('img/exploreLeft2.jpg')");
+	$("#goLeft1").click(function() {
+		$("#exploreBody").css("background-image", "url('img/troll.png')");
+		$("#goRight1").hide();
+		$("#goLeft1").hide();
+	});
+
+	$("#goRight2").click(function() {
+		$("#exploreBody").css("background-image", "url('img/troll.png')");
+		$("#goRight1").hide();
+		$("#goRight2").hide();
+		$("#goLeft1").hide();
+		$("#goLeft2").hide();
+	});
+
+	$("#goLeft2").click(function() {
+		$("#exploreBody").css("background-image", "url('img/three.jpg')");
+		$("#goRight1").hide();
+		$("#goRight2").hide();
+		$("#goRight3").show();
+		$("#goLeft1").hide();
 		$("#goLeft2").hide();
 		$("#goLeft3").show();
+		$("#goStraight1").show();
 	});
 
-	$("#goLeft-Straight2").click(function() {
-		$("#exploreBody").css("background-image", "url('img/exploreLeft3.jpg')");
+	$("#goRight2").click(function() {
+		$("#exploreBody").css("background-image", "url('img/troll.png')");
+		$("#goRight1").hide();
+		$("#goRight2").hide();
+		$("#goRight3").hide();
+		$("#goLeft1").hide();
+		$("#goLeft2").hide();
 		$("#goLeft3").hide();
-		$("#goBack").show();
+		$("#goStraight1").hide();
 	});
 
-
-	$("#goBack").click(function() {
-		$("#exploreBody").css("background-image", "url('img/exploreHome1.jpg')");
-		$("#goBack").hide();
-		$("#goRight1").show();
-		$("#goLeft1").show();
+	$("goLeft3").click(function() {
+		$("#exploreBody").css("background-image", "url('img/troll.png')");
+		$("#goRight1").hide();
+		$("#goRight2").hide();
+		$("#goRight3").hide();
+		$("#goLeft1").hide();
+		$("#goLeft2").hide();
+		$("#goLeft3").hide();
+		$("#goStraight1").hide();
 	});
 
-
+	$("goStraight1").click(function() {
+		$("#exploreBody").css("background-image", "url('img/troll.png')");
+		$("#goRight1").hide();
+		$("#goRight2").hide();
+		$("#goRight3").hide();
+		$("#goLeft1").hide();
+		$("#goLeft2").hide();
+		$("#goLeft3").hide();
+		$("#goStraight1").hide();
+	});
 
 });
